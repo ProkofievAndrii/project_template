b44117ac2511dd3775ce1910ea88798d4cd309a8
Prokofiev Andrii <prokofiev.main@gmail.com>
2024-03-22 15:03:41 +0200

    Added docstrings

--- app/io/input.py
+++ app/io/input.py
@@ -2,11 +2,27 @@ import pandas as pd
 
 
 def input_text_from_console():
+    """
+    Function to input text from the console.
+
+    Returns:
+        str: Text input by the user.
+    """
     text = input("Input text: ")
     return text
 
 
 def read_text_from_file_builtin(file_path):
+    """
+    Function to read text from a file using built-in capabilities.
+
+    Args:
+        file_path (str): The path to the file from which text will be read.
+
+    Returns:
+        str: Text read from the file.
+            Returns "Error: File not found" if the file is not found.
+    """
     try:
         with open(file_path, 'r') as file:
             text = file.read()
@@ -16,6 +32,16 @@ def read_text_from_file_builtin(file_path):
 
 
 def read_text_from_file_with_pandas(file_path):
+    """
+    Function to read text from a file using the pandas library.
+
+    Args:
+        file_path (str): The path to the file from which text will be read.
+
+    Returns:
+        str: Text read from the file.
+            Returns "Error: File not found" if the file is not found.
+    """
     try:
         data = pd.read_csv(file_path)
         text = data.iloc[:, 0].name
--- app/io/output.py
+++ app/io/output.py
@@ -1,8 +1,24 @@
 def print_text_to_console(text):
+    """
+    Function to print text to the console.
+
+    Args:
+        text (str): The text to be printed to the console.
+    """
     print(text)
 
 
 def write_text_to_file(file_path, text):
+    """
+    Function to write text to a file.
+
+    Args:
+        file_path (str): The path to the file where text will be written.
+        text (str): The text to be written to the file.
+
+    Returns:
+        str: Error message if file not found.
+    """
     try:
         with open(file_path, 'w') as file:
             file.write(text)
@@ -11,6 +27,16 @@ def write_text_to_file(file_path, text):
 
 
 def append_text_to_file(file_path, text):
+    """
+    Function to append text to a file.
+
+    Args:
+        file_path (str): The path to the file where text will be appended.
+        text (str): The text to be appended to the file.
+
+    Returns:
+        str: Error message if file not found.
+    """
     try:
         with open(file_path, 'a') as file:
             file.write(text)
